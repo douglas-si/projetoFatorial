9dbdfe249c6a254c8c602460c1e959c14051759c
Douglas Ferreira <douglas.fdd@gmail.com>
2015-11-09 22:14:30 -0200

    Refatoracao da classe TesteFatorial e inclusao de comentarios em Fatorial

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial{
-		
+	//Versao iterativa do calculo do Fatorial		
 	public static int calcularFatorial(int n){
 		int resultado = 1;
 		for(int i=2; i <=n; i++){
--- Testefatorial.java
+++ Testefatorial.java
@@ -1,8 +1,8 @@
 public class TesteFatorial {
 	public static void main(String[] args){
 		Fatorial fat = new Fatorial();
-		fat.calcularFatorial(5);
-		System.out.println(fat);	
+		int resultado = fat.calcularFatorial(5);
+		System.out.println(resultado);	
 	}
 
 }
