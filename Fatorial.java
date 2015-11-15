9926f3f786fb0e6a0d38578e15e0d4da3fae0b39
Douglas Ferreira <douglas.fdd@gmail.com>
2015-11-15 11:13:44 -0200

    Alteracao para o calulo de zero

--- Fatorial.java
+++ Fatorial.java
@@ -2,10 +2,12 @@ public class Fatorial{
 	//Versao iterativa do calculo do Fatorial		
 	public static int calcularFatorial(int n){
 		int resultado = 1;
-		for(int i=2; i <=n; i++){
-			resultado *= i;
-		}	
-		return resultado;
+		if (n != 0){
+			for(int i=2; i <=n; i++){
+				resultado *= i;
+			}	
+			return resultado;
+		}
 			
 	}
 }
